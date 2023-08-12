ace3e9621ac807d319bbfe5c21976f258ff05b1f
San Kim <waterfog9580@gmail.com>
2023-08-12 11:43:09 +0900

    :wrench: user-service db setup

--- bokeh-user-server/bokeh-user-bootstrap-api/build.gradle.kts
+++ bokeh-user-server/bokeh-user-bootstrap-api/build.gradle.kts
@@ -12,4 +12,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-aop")
+
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 }
---
+++ bokeh-user-server/bokeh-user-bootstrap-api/src/main/kotlin/com/waterfogsw/user/api/UserApiApplication.kt
@@ -0,0 +1,11 @@
+package com.waterfogsw.user.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UserApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<UserApiApplication>(*args)
+}
