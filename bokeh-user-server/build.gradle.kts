694c85198c025cd1ff924b6cc4c082c21523699e
San Kim <waterfog9580@gmail.com>
2023-08-12 20:50:43 +0900

    :wrench: Add Java 17 compatibility and adjust task configuration in build.gradle.kts
    This commit adds Java 17 as the source compatibility for the bokeh-user-server project. Along with this, the task configuration has been modified to use `KotlinCompile` instead of `org.jetbrains.kotlin.gradle.tasks.KotlinCompile`. The adjustments enhance code readability and ensures compatibility with the newly introduced java version. The `freeCompilerArgs` now includes strict options for `jsr305`. The project repositories configuration has also been nested within the `allprojects` for improved structure.

--- bokeh-user-server/build.gradle.kts
+++ bokeh-user-server/build.gradle.kts
@@ -1,26 +1,30 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
+    java
     id(Plugins.SPRING_BOOT) version PluginVersion.SPRING_BOOT_VERSION
     id(Plugins.SPRING_DEPENDENCY_MANAGEMENT) version PluginVersion.SPRING_DEPENDENCY_MANAGEMENT_VERSION
     id(Plugins.KOTLIN_JVM) version PluginVersion.KOTLIN_VERSION
     id(Plugins.KOTLIN_SPRING) version PluginVersion.KOTLIN_VERSION
 }
 
+java.sourceCompatibility = JavaVersion.VERSION_17
+
 allprojects {
     group = ProjectConfig.GROUP_NAME
     version = ProjectConfig.PROJECT_VERSION
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "17"
+        }
+    }
 
     repositories {
         mavenCentral()
     }
 
-    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-        kotlinOptions {
-            jvmTarget = "17"
-        }
-    }
-}
-
-subprojects {
     apply(plugin = Plugins.SPRING_BOOT)
     apply(plugin = Plugins.SPRING_DEPENDENCY_MANAGEMENT)
     apply(plugin = Plugins.KOTLIN_JVM)
