3c6ed1cabfd68d8a7f24d1aae0092bcf61f2cd12
San Kim <waterfog9580@gmail.com>
2023-08-12 21:48:17 +0900

    :wrench: Update Java version and refactor build.gradle.kts
    The 'bokeh-user-server/build.gradle.kts' file was updated to enhance project consistency and maintainability. Specifically, the "java" plugin was replaced with the "java-library" plugin. The Java source compatibility is also updated to version 17, encapsulating the setting inside a 'toolchain' block. This improves readability and provides a more explicit declaration of Java version in this Gradle Kotlin DSL script. The redundant parts (compile tasks and arguments for Kotlin compiler) were also removed as they are not needed anymore.

--- bokeh-user-server/build.gradle.kts
+++ bokeh-user-server/build.gradle.kts
@@ -1,25 +1,14 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    java
+    `java-library`
     id(Plugins.SPRING_BOOT) version PluginVersion.SPRING_BOOT_VERSION
     id(Plugins.SPRING_DEPENDENCY_MANAGEMENT) version PluginVersion.SPRING_DEPENDENCY_MANAGEMENT_VERSION
     id(Plugins.KOTLIN_JVM) version PluginVersion.KOTLIN_VERSION
     id(Plugins.KOTLIN_SPRING) version PluginVersion.KOTLIN_VERSION
 }
 
-java.sourceCompatibility = JavaVersion.VERSION_17
-
 allprojects {
     group = ProjectConfig.GROUP_NAME
     version = ProjectConfig.PROJECT_VERSION
-
-    tasks.withType<KotlinCompile> {
-        kotlinOptions {
-            freeCompilerArgs = listOf("-Xjsr305=strict")
-            jvmTarget = "17"
-        }
-    }
 
     repositories {
         mavenCentral()
@@ -29,4 +18,10 @@ allprojects {
     apply(plugin = Plugins.SPRING_DEPENDENCY_MANAGEMENT)
     apply(plugin = Plugins.KOTLIN_JVM)
     apply(plugin = Plugins.KOTLIN_SPRING)
+
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(17))
+        }
+    }
 }
