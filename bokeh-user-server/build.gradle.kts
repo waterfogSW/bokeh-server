da773b37927bc4460b42784cdef205ec8c1cc7c4
San Kim <waterfog9580@gmail.com>
2023-08-12 20:27:13 +0900

    :heavy_plus_sign: Update JVM target for Kotlin compile task
    Updated the Kotlin compile tasks in build.gradle.kts to target JVM version 17. This change is intended to take advantage of the latest JVM features and enhancements, whilst ensuring compatibility with the most recent version.

--- bokeh-user-server/build.gradle.kts
+++ bokeh-user-server/build.gradle.kts
@@ -12,6 +12,12 @@ allprojects {
     repositories {
         mavenCentral()
     }
+
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = "17"
+        }
+    }
 }
 
 subprojects {
