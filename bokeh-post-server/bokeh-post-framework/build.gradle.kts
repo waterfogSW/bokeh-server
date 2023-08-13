7e1d22b8f018844c9af91870e636aaefdc01b5ae
San Kim <waterfog9580@gmail.com>
2023-08-13 23:31:25 +0900

    :heavy_plus_sign: Added MongoDB dependency to build.gradle.kts
    In the bokeh-post-framework build.gradle.kts file, the spring-boot-starter-data-mongodb dependency was added. This was done to allow the project to work with MongoDB as its database.

--- bokeh-post-server/bokeh-post-framework/build.gradle.kts
+++ bokeh-post-server/bokeh-post-framework/build.gradle.kts
@@ -9,4 +9,6 @@ bootJar.enabled = false
 dependencies {
     implementation(project(":bokeh-post-domain"))
     implementation(project(":bokeh-post-application"))
+
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 }
