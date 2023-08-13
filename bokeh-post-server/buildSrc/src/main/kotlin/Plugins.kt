f9b14fca4d2616a179df91a9c8e4923ee1cf7720
San Kim <waterfog9580@gmail.com>
2023-08-13 23:30:13 +0900

    :adhesive_bandage: Remove unused KOTLIN_JPA plugin
    The KOTLIN_JPA plugin was removed from the Plugins.kt file as it was not being used. This change keeps the codebase clean and free of unnecessary dependencies.

--- bokeh-post-server/buildSrc/src/main/kotlin/Plugins.kt
+++ bokeh-post-server/buildSrc/src/main/kotlin/Plugins.kt
@@ -2,6 +2,5 @@ object Plugins {
     const val SPRING_BOOT = "org.springframework.boot"
     const val SPRING_DEPENDENCY_MANAGEMENT = "io.spring.dependency-management"
     const val KOTLIN_JVM = "org.jetbrains.kotlin.jvm"
-    const val KOTLIN_JPA = "org.jetbrains.kotlin.plugin.jpa"
     const val KOTLIN_SPRING = "org.jetbrains.kotlin.plugin.spring"
 }
