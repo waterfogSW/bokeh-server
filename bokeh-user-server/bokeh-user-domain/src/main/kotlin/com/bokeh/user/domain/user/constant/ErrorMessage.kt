ec7fbf5b441f78552de015e3105c85974c77e4b8
San Kim <waterfog9580@gmail.com>
2023-08-13 22:54:33 +0900

    :adhesive_bandage: Fix typo in password error message
    A typo was found and corrected in the constant for INVALID_PASSWORD_SET error message. The word "adn" was changed to the correct spelling "and". This change ensures accurate error messages to inform the user effectively.

--- bokeh-user-server/bokeh-user-domain/src/main/kotlin/com/bokeh/user/domain/user/constant/ErrorMessage.kt
+++ bokeh-user-server/bokeh-user-domain/src/main/kotlin/com/bokeh/user/domain/user/constant/ErrorMessage.kt
@@ -4,6 +4,6 @@ enum class ErrorMessage(
     val msg: String
 ) {
     INVALID_USERNAME_SET("Username should be between 3 and 10 characters and not blank."),
-    INVALID_PASSWORD_SET("Password should be between 8 adn 20 characters and not blank."),
+    INVALID_PASSWORD_SET("Password should be between 8 and 20 characters and not blank."),
     INVALID_EMAIL_SET("Email should be valid and not blank."),
 }
