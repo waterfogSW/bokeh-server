1d2de318bf0e9d8214fe223754b2943e8f98652e
San Kim <waterfog9580@gmail.com>
2023-08-13 18:06:19 +0900

    Set default timezone to Asia/Seoul in UserApiApplication
    In UserApiApplication.kt, default timezone has been set to Asia/Seoul. This update is made to make sure that times are displayed correctly for users in this timezone. Also, in application-local.yaml, the jdbc time zone property is set to Asia/Seoul for consistency with the application's primary timezone.

--- bokeh-user-server/bokeh-user-bootstrap-api/src/main/kotlin/com/bokeh/user/UserApiApplication.kt
+++ bokeh-user-server/bokeh-user-bootstrap-api/src/main/kotlin/com/bokeh/user/UserApiApplication.kt
@@ -2,10 +2,12 @@ package com.bokeh.user
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.*
 
 @SpringBootApplication
 class UserApiApplication
 
 fun main(args: Array<String>) {
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
     runApplication<UserApiApplication>(*args)
 }
