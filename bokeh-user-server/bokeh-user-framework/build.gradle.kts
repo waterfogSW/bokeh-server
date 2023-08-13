ef00bb7c91ec6f01faf88f02e71a9ffdeda6b133
San Kim <waterfog9580@gmail.com>
2023-08-13 17:36:40 +0900

    :sparkles: Add JPA support to Bokeh user service
    Updated the `build.gradle.kts` of bokeh-user-server and bokeh-user-framework to implement JPA support. The `spring-boot-starter-data-jpa` dependency was added to provide the necessary JPA classes and the Kotlin JPA plugin was included in the bokeh-user-server to facilitate the generation of no-arg constructors for JPA entities. This integration aims to simplify database operations within the application.

--- bokeh-user-server/bokeh-user-framework/build.gradle.kts
+++ bokeh-user-server/bokeh-user-framework/build.gradle.kts
@@ -9,6 +9,20 @@ bootJar.enabled = false
 dependencies {
     implementation(project(":bokeh-user-domain"))
     implementation(project(":bokeh-user-application"))
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     
     runtimeOnly("com.mysql:mysql-connector-j")
+}
+
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
+}
+
+noArg {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
 }
--- bokeh-user-server/build.gradle.kts
+++ bokeh-user-server/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id(Plugins.SPRING_BOOT) version PluginVersion.SPRING_BOOT_VERSION
     id(Plugins.SPRING_DEPENDENCY_MANAGEMENT) version PluginVersion.SPRING_DEPENDENCY_MANAGEMENT_VERSION
     id(Plugins.KOTLIN_JVM) version PluginVersion.KOTLIN_VERSION
+    id(Plugins.KOTLIN_JPA) version PluginVersion.KOTLIN_VERSION
     id(Plugins.KOTLIN_SPRING) version PluginVersion.KOTLIN_VERSION
 }
 
@@ -17,6 +18,7 @@ allprojects {
     apply(plugin = Plugins.SPRING_BOOT)
     apply(plugin = Plugins.SPRING_DEPENDENCY_MANAGEMENT)
     apply(plugin = Plugins.KOTLIN_JVM)
+    apply(plugin = Plugins.KOTLIN_JPA)
     apply(plugin = Plugins.KOTLIN_SPRING)
 
     java {
