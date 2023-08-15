35b13fbfab10fe556862aab1fbf9f4b01e5d6744
San Kim <waterfog9580@gmail.com>
2023-08-16 06:54:33 +0900

    Add Redis support to bokeh-user-framework and setup docker
    This commit adds Redis dependencies to the bokeh-user-framework's build.gradle.kts and the required image/configuration in docker-compose for bokeh-user-bootstrap-api. Specifically, `spring-boot-starter-data-redis` and `lettuce-core` were added, and a new Redis service was added to the docker-compose file. This was done to enable caching and improve the performance of bokeh-user-server.

--- bokeh-user-server/bokeh-user-framework/build.gradle.kts
+++ bokeh-user-server/bokeh-user-framework/build.gradle.kts
@@ -10,9 +10,13 @@ dependencies {
     implementation(project(":bokeh-user-domain"))
     implementation(project(":bokeh-user-application"))
 
+    // db
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    
     runtimeOnly("com.mysql:mysql-connector-j")
+
+    // redis
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("io.lettuce:lettuce-core:6.2.4.RELEASE")
 }
 
 allOpen {
