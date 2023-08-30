6445c368ccb4099bbb2aad70a941397549d6673a
San Kim <waterfog9580@gmail.com>
2023-08-30 10:32:38 +0900

    :heavy_plus_sign: Add test dependencies to build.gradle.kts
    Added several testing libraries, including MockK, Kotest, SpringMockK and Spring restdocs to the build.gradle.kts file. These libraries will support unit testing and documentation generation for the project.

--- bokeh-user-server/build.gradle.kts
+++ bokeh-user-server/build.gradle.kts
@@ -21,6 +21,16 @@ allprojects {
     apply(plugin = Plugins.KOTLIN_JPA)
     apply(plugin = Plugins.KOTLIN_SPRING)
 
+    dependencies {
+        testImplementation("io.mockk:mockk:1.13.7")
+        testImplementation("io.kotest:kotest-runner-junit5:5.6.0")
+        testImplementation("io.kotest:kotest-assertions-core:5.6.0")
+        testImplementation("com.ninja-squad:springmockk:4.0.2")
+        testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+        testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+    }
+
     java {
         toolchain {
             languageVersion.set(JavaLanguageVersion.of(17))
