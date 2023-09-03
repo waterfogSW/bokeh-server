5415bac28beaba8323188b66bea8b1b08fbd81b3
San Kim <waterfog9580@gmail.com>
2023-09-03 12:07:06 +0900

    :sparkles: Add PostApiApplication with Asia/Seoul timezone setting
    This commit introduces a new spring boot application, PostApiApplication, in the package com.bokeh.post. The main function specifies 'Asia/Seoul' as the default timezone. This change is essential to ensure timestamp consistency in all posts given the application's primary users are based in Seoul.

---
+++ bokeh-post-server/bokeh-post-bootstrap-api/src/main/kotlin/com/bokeh/post/PostApiApplication.kt
@@ -0,0 +1,14 @@
+package com.bokeh.post
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import java.util.*
+
+@SpringBootApplication
+class PostApiApplication
+
+fun main(args: Array<String>) {
+    TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
+    runApplication<PostApiApplication>(*args)
+}
