fd50cc4d4cb6b18c61ec948b56a346bf205aa60c
San Kim <waterfog9580@gmail.com>
2023-09-03 12:06:44 +0900

    :heavy_plus_sign: Add Spring Boot dependencies for webflux, validation, actuator, aop, devtools, and docker-compose
    The above dependencies were added to the build.gradle.kts file to enhance the functionality of the Bokeh Post server. With these additions, it supports webflux for non-blocking model, validation for input data, actuator for management endpoints, aop for crosscutting concerns, and devtools along with docker compose for development utilities.

--- bokeh-post-server/bokeh-post-bootstrap-api/build.gradle.kts
+++ bokeh-post-server/bokeh-post-bootstrap-api/build.gradle.kts
@@ -4,4 +4,12 @@ dependencies {
     implementation(project(":bokeh-post-domain"))
     implementation(project(":bokeh-post-application"))
     implementation(project(":bokeh-post-framework"))
+
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-aop")
+
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 }
