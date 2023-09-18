0df259e6a845fe07686a23346fabc9f9710018de
San Kim <waterfog9580@gmail.com>
2023-09-18 17:23:16 +0900

    :recycle: Merge UserAuthRestController into UserRestController
    Deleted UserAuthRestController as its functionality has been merged into UserRestController. This is to streamline API design, reducing the number of controller classes without losing functionality. User login method is now present in UserRestController. Handling different user actions under a single controller brings more coherence to the code.

--- user-service/api/src/main/kotlin/com/bokeh/user/api/user/controller/UserRestController.kt
+++ user-service/api/src/main/kotlin/com/bokeh/user/api/user/controller/UserRestController.kt
@@ -1,7 +1,10 @@
 package com.bokeh.user.api.user.controller
 
+import com.bokeh.user.api.user.dto.UserAuthLoginRequest
+import com.bokeh.user.api.user.dto.UserAuthLoginResponse
 import com.bokeh.user.api.user.dto.UserCreateRequest
 import com.bokeh.user.api.user.dto.UserInfoResponse
+import com.bokeh.user.application.user.usecase.UserAuthCommandUseCase
 import com.bokeh.user.application.user.usecase.UserCommandUseCase
 import com.bokeh.user.application.user.usecase.UserQueryUseCase
 import org.springframework.http.HttpStatus
@@ -9,10 +12,10 @@ import org.springframework.web.bind.annotation.*
 import java.util.*
 
 @RestController
-@RequestMapping("/users")
 class UserRestController(
     private val userQueryUseCase: UserQueryUseCase,
     private val userCommandUseCase: UserCommandUseCase,
+    private val userAuthCommandUseCase: UserAuthCommandUseCase,
 ) {
 
     @GetMapping("{id}")
@@ -26,6 +29,16 @@ class UserRestController(
     @ResponseStatus(HttpStatus.CREATED)
     fun createUser(@RequestBody userCreateRequest: UserCreateRequest) {
         userCommandUseCase.createUser(param = userCreateRequest.toParam())
+    }
+
+    @PostMapping("login")
+    @ResponseStatus(HttpStatus.OK)
+    fun login(@RequestBody userAuthLoginRequest: UserAuthLoginRequest) : UserAuthLoginResponse {
+        val userTokens = userAuthCommandUseCase.login(
+            email = userAuthLoginRequest.email,
+            password = userAuthLoginRequest.password,
+        )
+        return UserAuthLoginResponse.from(userTokens = userTokens)
     }
 
 }
