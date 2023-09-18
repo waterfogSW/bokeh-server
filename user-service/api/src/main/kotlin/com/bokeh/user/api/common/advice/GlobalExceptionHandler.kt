cc073c0d001dd3ebc66901b01265f452df468ba5
San Kim <waterfog9580@gmail.com>
2023-09-18 18:04:04 +0900

    :sparkles: Add GlobalExceptionHandler class to handle exceptions
    A new class named GlobalExceptionHandler has been added under the package com.bokeh.user.api.user.common.advice. This class will manage exceptions like NotFoundException and generic Exception through dedicated methods. These methods handle exceptions and log the error messages, while returning suitable HTTP Status codes.

---
+++ user-service/api/src/main/kotlin/com/bokeh/user/api/common/advice/GlobalExceptionHandler.kt
@@ -0,0 +1,29 @@
+package com.bokeh.user.api.common.advice
+
+import com.bokeh.user.framework.common.exception.NotFoundException
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    private val log = LoggerFactory.getLogger(GlobalExceptionHandler::class.java)
+
+    @ExceptionHandler(value = [NotFoundException::class])
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    fun handleNotFoundException(e: NotFoundException): String {
+        log.error("Not Found Exception: ${e.message}")
+        return "Not Found Error: ${e.message}"
+    }
+
+    @ExceptionHandler(value = [RuntimeException::class])
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    fun handleException(e: RuntimeException): String {
+        log.debug("Internal Server Error: ${e.message}")
+        return "Internal Server Error : ${e.message}"
+    }
+
+}
