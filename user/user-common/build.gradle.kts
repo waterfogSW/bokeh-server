e0224b52335a314d1cc2d150f816d0947e100a66
San Kim <waterfog9580@gmail.com>
2023-09-29 12:22:44 +0900

    :wrench: Update Gradle dependencies and configurations
    This commit upgrades the SLF4j API dependency to version 2.0.5 and updates existing plugins in the build.gradle.kts. The changes also involve adjusting all project configurations, making updates to align better with Spring Boot and Kotlin task versions. This was done to take advantage of the new features and improvements these newer versions offer. Additionally, the structure of the build files has been slightly reorganized for readability and to align with Kotlin DSL syntax requirements.

--- user/user-common/build.gradle.kts
+++ user/user-common/build.gradle.kts
@@ -1,11 +1,11 @@
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
-dependencies {
-    implementation("org.slf4j:slf4j-api:1.7.32")
-}
-
 val jar: Jar by tasks
 val bootJar: BootJar by tasks
 
 jar.enabled = true
 bootJar.enabled = false
+
+dependencies {
+    implementation("org.slf4j:slf4j-api:2.0.5")
+}
