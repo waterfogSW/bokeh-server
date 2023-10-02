2d0e08dbd55ac3e6c8d7e0d904d2de5d5339be9e
San Kim <waterfog9580@gmail.com>
2023-10-02 21:27:23 +0900

    :heavy_plus_sign: Add dependencies to Kafka adapter build script
    In this update, new dependencies have been added to the Kafka adapter's build script. This change was made to ensure that the necessary projects and Spring's Kafka library are incorporated into the build, thus enabling better interaction with Kafka during runtime.

--- post/post-adapter/kafka/build.gradle.kts
+++ post/post-adapter/kafka/build.gradle.kts
@@ -2,3 +2,11 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 val jar: Jar by tasks
 val bootJar: BootJar by tasks
+
+dependencies {
+    implementation(project(":post-common"))
+    implementation(project(":post-domain"))
+    implementation(project(":post-application"))
+
+    implementation("org.springframework.kafka:spring-kafka:3.0.11")
+}
