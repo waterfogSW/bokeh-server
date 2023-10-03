3fe8f04704188a81b8723fd4ab228998d36b73fc
San Kim <waterfog9580@gmail.com>
2023-10-03 14:47:30 +0900

    :wrench: Enable jar and disable bootJar in build scripts
    Enabled the jar task and disabled the bootJar task in both post-adapter/kafka and post-adapter/mongo build.gradle files. This was done to ensure that only the jar package is created when the build is run, not the bootJar, to align with the project's packaging requirements.

--- post/post-adapter/kafka/build.gradle.kts
+++ post/post-adapter/kafka/build.gradle.kts
@@ -3,6 +3,9 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 val jar: Jar by tasks
 val bootJar: BootJar by tasks
 
+jar.enabled = true
+bootJar.enabled = false
+
 dependencies {
     implementation(project(":post-common"))
     implementation(project(":post-domain"))
--- post/post-adapter/mongo/build.gradle.kts
+++ post/post-adapter/mongo/build.gradle.kts
@@ -2,3 +2,6 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 val jar: Jar by tasks
 val bootJar: BootJar by tasks
+
+jar.enabled = true
+bootJar.enabled = false
