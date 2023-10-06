4cb0fd125db2de3957d7994b1166572588b89765
San Kim <waterfog9580@gmail.com>
2023-10-06 18:01:50 +0900

    :heavy_plus_sign: Add Spring WebSocket dependency to build.gradle.kts
    An implementation of "

--- post/post-api/websocket/build.gradle.kts
+++ post/post-api/websocket/build.gradle.kts
@@ -4,6 +4,7 @@ dependencies {
     implementation(project(":post-application"))
     implementation(project(":post-adapter-kafka"))
 
+    implementation("org.springframework:spring-websocket")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-aop")
