49dd91305d9e5d532c08b8bf3cc7b788ed63c8e4
San Kim <waterfog9580@gmail.com>
2023-10-09 16:41:22 +0900

    :recycle: Refactor logging in PostKafka classes
    The responsibility of logging send events was moved from `PostKafkaAdapter` to `PostKafkaProducer`. This change makes the code cleaner by separating the concerns more distinctly - `PostKafkaAdapter` focuses on converting `Post` domain objects to `PostCreatEvent` DTOs while `PostKafkaProducer` manages sending these events to Kafka and logging related activities.

--- post/post-adapter/kafka/src/main/kotlin/com/bokeh/post/adapter/kafka/adapter/PostKafkaAdapter.kt
+++ post/post-adapter/kafka/src/main/kotlin/com/bokeh/post/adapter/kafka/adapter/PostKafkaAdapter.kt
@@ -4,7 +4,6 @@ import com.bokeh.post.adapter.kafka.common.constant.KafkaTopic
 import com.bokeh.post.adapter.kafka.dto.PostCreatEvent
 import com.bokeh.post.application.post.port.out.PostEventPort
 import com.bokeh.post.domain.post.domain.Post
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
@@ -12,14 +11,9 @@ class PostKafkaAdapter(
     private val postKafkaProducer: PostKafkaProducer
 ) : PostEventPort {
 
-    private val log = LoggerFactory.getLogger(javaClass)
-
     override fun sendCreateEvent(post: Post) {
         val event: PostCreatEvent = PostCreatEvent.from(post)
-        postKafkaProducer
-            .send(topic = KafkaTopic.POST_CREATE, key = event.id, value = event)
-            .doOnSuccess { log.info("Send Post Create Event(Event ID : ${event.id})") }
-            .subscribe()
+        postKafkaProducer.send(topic = KafkaTopic.POST_CREATE, key = event.id, value = event)
     }
 
 }
--- post/post-adapter/kafka/src/main/kotlin/com/bokeh/post/adapter/kafka/adapter/PostKafkaProducer.kt
+++ post/post-adapter/kafka/src/main/kotlin/com/bokeh/post/adapter/kafka/adapter/PostKafkaProducer.kt
@@ -1,10 +1,9 @@
 package com.bokeh.post.adapter.kafka.adapter
 
 import com.bokeh.post.adapter.kafka.common.constant.KafkaTopic
+import org.slf4j.LoggerFactory
 import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate
 import org.springframework.stereotype.Component
-import reactor.core.publisher.Mono
-import reactor.kafka.sender.SenderResult
 import java.util.*
 
 @Component
@@ -12,11 +11,12 @@ class PostKafkaProducer(
     private val reactiveKafkaProducerTemplate: ReactiveKafkaProducerTemplate<UUID, Any>,
 ) {
 
-    fun send(
-        topic: KafkaTopic,
-        key: UUID,
-        value: Any
-    ): Mono<SenderResult<Void>> {
-        return reactiveKafkaProducerTemplate.send(topic.name, key, value)
+    private val log = LoggerFactory.getLogger(this::class.java)
+
+    fun send(topic: KafkaTopic, key: UUID, value: Any) {
+        reactiveKafkaProducerTemplate
+            .send(topic.name, key, value)
+            .doOnSuccess { log.info("Send Post Create Event(Event ID : $key)") }
+            .subscribe()
     }
 }
