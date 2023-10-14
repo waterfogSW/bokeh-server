d3172f6de902bfbfb3855de95312a81264ccc9f1
San Kim <waterfog9580@gmail.com>
2023-10-14 16:13:17 +0900

    :adhesive_bandage: Add RequestMapping annotation to PostRestController
    In order to have a more clean and understandable api endpoint, a RequestMapping annotation declaring the base url "posts" is added to the PostRestController class. Now, all the endpoints defined in this class will start with "/posts".

--- post/post-api/rest/src/main/kotlin/com/bokeh/post/api/rest/controller/PostRestController.kt
+++ post/post-api/rest/src/main/kotlin/com/bokeh/post/api/rest/controller/PostRestController.kt
@@ -5,10 +5,12 @@ import com.bokeh.post.application.post.port.`in`.command.PostCreateCommandUseCas
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
 
 @RestController
+@RequestMapping("posts")
 class PostRestController(
     private val postCreateCommandUseCase: PostCreateCommandUseCase,
 ) {
